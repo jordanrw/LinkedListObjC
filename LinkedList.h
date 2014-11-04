c4243edfe010463d71d4d29ede3c74f0c2f78148
Jordan White <jordanrw@vt.edu>
2014-11-04 15:11:05 -0500

    initial commit

---
+++ LinkedList.h
@@ -0,0 +1,13 @@
+//
+//  LinkedList.h
+//  LinkedList
+//
+//  Created by Jordan White on 11/4/14.
+//  Copyright (c) 2014 Option White. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LinkedList : NSObject
+
+@end
---
+++ LinkedList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkedList
+//
+//  Created by Jordan White on 11/4/14.
+//  Copyright (c) 2014 Option White. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
